97a6cdbb3834d97afa3528056debdaaac0831764
ezailrak <raknante@gmail.com>
2023-10-02 12:33:04 +0300

    add comments

--- src/components/UI/dice.tsx
+++ src/components/UI/dice.tsx
@@ -11,25 +11,29 @@ interface DiceProps {
 }
 
 const Dice: React.FC<DiceProps> = ({ players, totalRounds, diceCount }) => {
+	// Define and initialize state variables using the useState hook
   const [diceValues, setDiceValues] = useState<number[]>(
     Array(diceCount).fill(0)
-  ); // dice values init to 0
-  const [currentPlayerIndex, setCurrentPlayerIndex] = useState<number>(0); //index of the player who is playing now
-  const [scores, setScores] = useState<number[]>(Array(players.length).fill(0)); 
-  const [round, setRound] = useState<number>(1); // 1 round have players.length roll
-  const [rollCount, setRollCount] = useState<number>(0); //use to count the number of player who's roll dice in a round
-  const [rollDiceDisabled, setRollDiceDisabled] = useState<boolean>(false);
-  const [gameOver, setGameOver] = useState<boolean>(false);
-  const [winner, setWinner] = useState<string | null>(null);
-
+  ); // Initialize dice values to an array of zeros
+  const [currentPlayerIndex, setCurrentPlayerIndex] = useState<number>(0);// Index of the player who is playing now
+  const [scores, setScores] = useState<number[]>(Array(players.length).fill(0));  // Initialize scores to an array of zeros
+  const [round, setRound] = useState<number>(1); // Initialize the current round to 1
+  const [rollCount, setRollCount] = useState<number>(0); // Used to count the number of players who've rolled dice in a round
+  const [rollDiceDisabled, setRollDiceDisabled] = useState<boolean>(false); // Indicates if the "Roll Dice" button should be disabled
+  const [gameOver, setGameOver] = useState<boolean>(false); // Indicates if the game is over
+  const [winner, setWinner] = useState<string | null>(null); // Stores the name of the winner
+	
+	  // Function to randomly select the first player
 	const defineFirstPlayerIndex = () => {
-		// definir aléatoirement le premier joueur
 		setCurrentPlayerIndex(Math.floor(Math.random() * players.length))
 
 	}
+
+	// Function to simulate rolling dice
   const rollDice = () => {
 		setRollDiceDisabled(true)
 
+		// Generate random dice values
 		const newDiceValues = Array(diceCount)
 		.fill(0)
 		.map(() => Math.floor(Math.random() * 6) + 1);
@@ -38,6 +42,7 @@ const Dice: React.FC<DiceProps> = ({ players, totalRounds, diceCount }) => {
     setRollCount(rollCount + 1);
   };
 
+	// Function to update scores based on rolled dice values
 	const updateScores = () => {
 		const newScores = [...scores];
 		const roundScore = diceValues.reduce((sum, value) => sum + value, 0);
@@ -47,7 +52,7 @@ const Dice: React.FC<DiceProps> = ({ players, totalRounds, diceCount }) => {
 
 
 		if (rollCount >= players.length && round >= totalRounds) {
-			// game over, pick the winner
+			// Check if the game is over and determine the winner
 			const maxScore = Math.max(...newScores);
 			const winningPlayers = players.filter(
 				(_, index) => newScores[index] === maxScore
@@ -55,24 +60,26 @@ const Dice: React.FC<DiceProps> = ({ players, totalRounds, diceCount }) => {
 			if (winningPlayers.length === 1) {
 				setWinner(winningPlayers[0]);
 			} else {
-				setWinner("Égalité !");
+				setWinner("Égalité !"); // In case of a tie
 			}
 			setGameOver(true)
 		}
 	};
 
+	// Function to switch to the next player's turn
 	const nextPlayer = () => {
 		setCurrentPlayerIndex((currentPlayerIndex + 1) % players.length);
 		setDiceValues(Array(diceCount).fill(0));
 		setRollDiceDisabled(false)
 		if(rollCount >= players.length){
-			// go to the next round
+			// Move to the next round
 			setRollCount(0)
 			setRound(round + 1);
 			console.log('newScores',scores)
 		}
 	}
  
+	// Function to reset the game to its initial state
 	const resetGame = () => {
 		defineFirstPlayerIndex()
 		setDiceValues(Array(diceCount).fill(0));
@@ -83,8 +90,8 @@ const Dice: React.FC<DiceProps> = ({ players, totalRounds, diceCount }) => {
 		setRollDiceDisabled(false)
 		setGameOver(false)
 	}
-
- const renderDiceImages = () => {
+	// Function to render dice images based on the rolled values
+  const renderDiceImages = () => {
     return diceValues.map((diceValue, index) => (
       <img
         key={index}
@@ -96,6 +103,7 @@ const Dice: React.FC<DiceProps> = ({ players, totalRounds, diceCount }) => {
     ));
   };
 
+	// useEffect to update scores when rollCount changes
   useEffect(() => {
 		console.log('nga tonga ida ato')
 		if(rollCount > 0) {
@@ -104,7 +112,7 @@ const Dice: React.FC<DiceProps> = ({ players, totalRounds, diceCount }) => {
   }, [rollCount]);
 	
 
-
+	// Render the Dice component
   return (
 		<div style={{display: 'flex', justifyContent: 'center'}}>
 			<div style={{paddingTop: '10px'}}>
